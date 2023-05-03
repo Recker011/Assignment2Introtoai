e03872f41bb387c7375a55acb81c2397374e179e
Recker011 <randewkumarasinghe@gmail.com>
2023-05-03 18:21:30 +1000

    Fixed text file

--- testbed.py
+++ testbed.py
@@ -1,12 +1,13 @@
 def parse_input_file(filename):
     with open(filename, 'r') as f:
         lines = f.read().splitlines()
-        tell = lines[1]
-        ask = lines[4]
+        lines = [line.replace(' ', '').strip() for line in lines]
+        t, a = lines[1].split(';'), lines[3].split(';')
+        tell, ask = list(filter(None, t)), list(filter(None, a))
         return tell, ask
 
 def checkparsein():
     line1, line2 = parse_input_file("C:\\Users\\Randew Kumarasinghe\\Desktop\\COS30019\\Assignment2Introtoai\\Assignment2Introtoai\\test.txt")
-    print(line1, line2)
+    print(line1[0])
     
 checkparsein()
