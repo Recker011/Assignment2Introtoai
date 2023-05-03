41b84786a28e6b9d805ff86e168bfea26cf55886
Recker011 <randewkumarasinghe@gmail.com>
2023-05-03 19:01:24 +1000

    Implemented Truth table checking algorithm
    Added functionality to the truth table checking algorithm, possible issue might be where in Horn Clause text files parsing will work on that

--- TTchecking.py
+++ TTchecking.py
@@ -1,8 +1,56 @@
+from itertools import product
+
 class TTchecking:
-    def __init__(self,kb,query):
+    def __init__(self, kb, query):
         self.kb = kb
         self.query = query
-        
-        def check(self):
-            # implementation of the Truth Table checking function
-            pass
+        self.symbols = self.get_symbols(kb + [query])
+    
+    # Extract all the symbols (propositional variables) from the list.
+    def get_symbols(self, sentences):
+        symbols = set()
+        for sentence in sentences:
+            for char in sentence:
+                if char.isalpha() and char not in symbols:
+                    symbols.add(char)
+        return sorted(list(symbols))
+    
+    # Evaluates whether a given sentence is true under a given model.
+    def pl_true(self, sentence, model):
+        if sentence == 'True':
+            return True
+        elif sentence == 'False':
+            return False
+        elif len(sentence) == 1:
+            return model[sentence]
+        elif '=>' in sentence:
+            lhs, rhs = sentence.split('=>')
+            return (not self.pl_true(lhs, model)) or self.pl_true(rhs, model)
+        elif '&' in sentence:
+            lhs, rhs = sentence.split('&')
+            return self.pl_true(lhs, model) and self.pl_true(rhs, model)
+        elif '|' in sentence:
+            lhs, rhs = sentence.split('|')
+            return self.pl_true(lhs, model) or self.pl_true(rhs, model)
+        elif '<=>' in sentence:
+            lhs, rhs = sentence.split('<=>')
+            return self.pl_true(lhs, model) == self.pl_true(rhs, model)
+        elif '~' in sentence:
+            negated_sentence = sentence[1:]
+            return not self.pl_true(negated_sentence, model)
+    
+    # Recursively checks all the possible models to determine if the query can be inferred from the knowledge base
+    def check_all(self, kb, query, symbols, model):
+        if not symbols:
+            if all(self.pl_true(kb_clause, model) for kb_clause in kb):
+                return self.pl_true(query, model)
+            else:
+                return True
+        else:
+            p = symbols[0]
+            rest = symbols[1:]
+            return (self.check_all(kb, query, rest, {**model, p: True}) and
+                    self.check_all(kb, query, rest, {**model, p: False}))
+    
+    def check(self):
+        return self.check_all(self.kb, self.query, self.symbols.copy(), {})
--- testbed.py
+++ testbed.py
@@ -1,13 +1,33 @@
+from TTchecking import TTchecking
+
+
 def parse_input_file(filename):
-    with open(filename, 'r') as f:
+    with open(filename, "r") as f:
         lines = f.read().splitlines()
-        lines = [line.replace(' ', '').strip() for line in lines]
-        t, a = lines[1].split(';'), lines[3].split(';')
+        lines = [line.replace(" ", "").strip() for line in lines]
+        t, a = lines[1].split(";"), lines[3].split(";")
         tell, ask = list(filter(None, t)), list(filter(None, a))
         return tell, ask
 
+
 def checkparsein():
-    line1, line2 = parse_input_file("C:\\Users\\Randew Kumarasinghe\\Desktop\\COS30019\\Assignment2Introtoai\\Assignment2Introtoai\\test.txt")
-    print(line1[0])
+    line1, line2 = parse_input_file(
+        "C:\\Users\\Randew Kumarasinghe\\Desktop\\COS30019\\Assignment2Introtoai\\Assignment2Introtoai\\test.txt"
+    )
+    print(line1, line2)
+
+
+def checkTTcheck():
+    kb, query = parse_input_file(
+        "C:\\Users\\Randew Kumarasinghe\\Desktop\\COS30019\\Assignment2Introtoai\\Assignment2Introtoai\\test.txt"
+    )
+    tt = TTchecking(kb, query[0])
+    result = tt.check()
+    print(result)
     
-checkparsein()
+    
+checkTTcheck()
+
+
+
+
