ac92128448ab3ee557077dc6aed1075b6b7045e5
Recker011 <randewkumarasinghe@gmail.com>
2023-05-04 14:38:54 +1000

    minor updates
    Added a small efficiency patch to parsing function
    small update to testing function

--- testbed.py
+++ testbed.py
@@ -1,12 +1,37 @@
 def parse_input_file(filename):
     with open(filename, 'r') as f:
         lines = f.read().splitlines()
+<<<<<<< Updated upstream
         tell = lines[1]
         ask = lines[3]
         return tell, ask
+=======
+        tell_line = lines[1].replace(" ", "")
+        ask_line = lines[3].replace(" ", "")
+        tell = tell_line.split(";")
+        ask = [ask_line]
+        return tell[:-1], ask
+>>>>>>> Stashed changes
 
 def checkparsein():
     line1, line2 = parse_input_file("C:\\Users\\Randew Kumarasinghe\\Desktop\\COS30019\\Assignment2Introtoai\\Assignment2Introtoai\\test.txt")
     print(line1, line2)
+<<<<<<< Updated upstream
+=======
+
+
+def checkTTcheck():
+    filename = "test.txt"
+    print(f"Reading input file: {filename}")
+    kb, query = parse_input_file(filename)
+    print(f"kb: {kb}")
+    print(f"query: {query}")
+    if not query:
+        print("Error: query list is empty")
+        return
+    tt = TTchecking(kb, query[0])
+    result = tt.check()
+    print(result)
+>>>>>>> Stashed changes
     
 checkparsein()
