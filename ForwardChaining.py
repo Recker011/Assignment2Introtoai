16e029de657ab5c6006b5c5e2793502fb7993430
Recker011 <randewkumarasinghe@gmail.com>
2023-05-08 06:52:14 +1000

    Implemented Backward chaining
    Requires testing but its an implementation

--- ForwardChaining.py
+++ ForwardChaining.py
@@ -1,8 +1,33 @@
+from Sentence import Sentence
+
 class ForwardChaining:
-    def initialize(self,kb,query):
+    def __init__(self, kb, query):
         self.kb = kb
         self.query = query
-        
-        def check(self):
-            #Implementation of Forward chaining algorithm
-            pass
+
+    def check(self):
+        count = {}
+        inferred = {}
+        agenda = []
+
+        for clause in self.kb:
+            if len(clause.subclauses) == 0:
+                agenda.append(clause.clause)
+            else:
+                count[clause] = len(clause.subclauses)
+
+        while agenda:
+            p = agenda.pop(0)
+            if p == self.query.clause:
+                return 'YES'
+            if p not in inferred:
+                inferred[p] = True
+                for clause in self.kb:
+                    if p in [subclause.clause for subclause in clause.subclauses]:
+                        count[clause] -= 1
+                        if count[clause] == 0:
+                            if clause.connective == '=>':
+                                agenda.append(clause.subclauses[-1].clause)
+                            else:
+                                agenda.append(clause.clause)
+        return 'NO'
--- testbed.py
+++ testbed.py
@@ -1,25 +1,29 @@
 from TTchecking import TTchecking
+from BackwardChaining import BackwardChaining
 from Sentence import Sentence
 import sys
-
+import re
 
 def parse_input_file(filename):
     try:
         with open(filename, 'r') as f:
             lines = f.read().splitlines()
             lines = [line.replace(' ', '').strip() for line in lines]
-            tell_list, ask_list = lines[1].split(';'), lines[3].split(';')
-            tell, ask = list(filter(None, tell_list)), list(filter(None, ask_list))
+            tell_list = re.split(r';\s*(?![^()]*\))', lines[1])
+            ask_list = lines[3].split(';')
+            tell = list(filter(None, tell_list))
+            ask = list(filter(None, ask_list))
             kb = [Sentence(sentence) for sentence in tell]
             query = Sentence(ask[0])
             return kb, query
-    except FileNotFoundError: # Error handling for file not found
+    except FileNotFoundError:
+        # Error handling for file not found
         print(f"Error: file {filename} not found.")
         sys.exit(1)
-    except IndexError: # error handling for wrong formatting of input text
+    except IndexError:
+        # error handling for wrong formatting of input text
         print(f"Error: file {filename} is not in the correct format.")
         sys.exit(1)
-
 
 def checkparsein():
     line1, line2 = parse_input_file(
@@ -27,23 +31,33 @@ def checkparsein():
     )
     print(line1, line2)
 
-
 def checkTTcheck():
-    filename = "test_HornKB.txt"
-    print(f"Reading input file: {filename}")
+    algorithm = "TTcheck"
+    filename = "test_genericKB.txt"
+    print(f"Reading input file: {filename}   |||||   algorithm: {algorithm}")
     kb, query = parse_input_file(filename)
-   # print(f"kb: {kb}")
-   # print(f"query: {query}")
+    print(f"kb: {kb}")
+    print(f"query: {query}")
     if not query:
         print("Error: query list is empty")
         return
     tt = TTchecking(kb, query)
     result = tt.check()
     print(result)
-    
-    
-checkTTcheck()
 
+def checkBCcheck():
+    algorithm = 'BCcheck'
+    filename = "test_genericKB.txt"
+    print(f"Reading input file: {filename}    |||||   algorithm: {algorithm}")
+    kb, query = parse_input_file(filename)
+    print(f"kb: {kb}")
+    print(f"query: {query}")
+    if not query:
+        print("Error: query list is empty")
+        return
+    bc = BackwardChaining(kb, query)
+    result = bc.check()
+    print(result)
 
-
-
+#checkTTcheck()
+checkBCcheck()
