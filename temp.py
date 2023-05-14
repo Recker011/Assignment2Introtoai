4e3cd8324cce3d98a6645723499386f16aefa7be
Recker011 <randewkumarasinghe@gmail.com>
2023-05-15 03:20:16 +1000

    Update temp.py

--- temp.py
+++ temp.py
@@ -208,3 +208,38 @@ def parse_input_file(filename):
 kb, query = parse_input_file('test_HornKB.txt')
 print('KB:', kb)
 print('Query:', query)
+
+
+# Rete algorithm
+
+def rete(KB, query):
+    def implies(fact):
+        return fact.split('=>')[1] if '=>' in fact else None
+
+    def entails(fact):
+        return set(fact.split('=>')[0].split('&')) if '=>' in fact else set()
+
+    def is_fact(fact):
+        return '=>' not in fact
+
+    agenda = [fact for fact in KB if is_fact(fact)]
+    inferred = {}
+    entailed_facts = []
+    while agenda:
+        p = agenda.pop(0)
+        if p == query[0]:
+            entailed_facts.append(p)
+            return f'YES: {", ".join(entailed_facts)}'
+        inferred[p] = True
+        for fact in KB:
+            premises = entails(fact)
+            if all(inferred.get(premise) for premise in premises):
+                q = implies(fact)
+                if q and not inferred.get(q):
+                    entailed_facts.append(q)
+                    agenda.append(q)
+    return 'NO'
+
+KB = ['p2=>p3', 'p3=>p1', 'c=>e', 'b&e=>f', 'f&g=>h', 'p1=>d', 'p1&p3=>c', 'a', 'b', 'p2']
+query = ['d']
+print(rete(KB, query))
