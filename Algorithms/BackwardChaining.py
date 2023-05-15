02e3add7f3bd9f491450d8404b3f18bcd1b8acbb
Recker011 <randewkumarasinghe@gmail.com>
2023-05-16 00:11:53 +1000

    Update BackwardChaining.py

--- Algorithms/BackwardChaining.py
+++ Algorithms/BackwardChaining.py
@@ -1,5 +1,4 @@
 class BackwardChaining:
-    
     @staticmethod
     def check(KB, q):
         # Initialize the inferred dictionary
@@ -15,13 +14,14 @@ class BackwardChaining:
         # Call the recursive BC function
         result = BackwardChaining.BC(KB, q[0], inferred, entailed)
         if result:
-            return "YES: " + ", ".join(entailed) # outputs the YES and order of entailed facts
+            return "YES: " + ", ".join(entailed)  # outputs the YES and order of entailed facts
         else:
             return "NO"
 
+    @staticmethod
     def BC(KB, q, inferred, entailed):
-        # Check if the query is already known to be true
-        if inferred[q]:
+        # Check if the query is already known to be true or if it's a fact in the KB
+        if inferred[q] or q in KB:
             return True
         # Find all implications with q as the conclusion
         implications = [clause for clause in KB if "=>" in clause and clause.split("=>")[1].strip() == q]
@@ -43,3 +43,4 @@ class BackwardChaining:
                 inferred[q] = True
                 return True
         return False
+
